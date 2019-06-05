6634ee055842efc69aee01ca3ac96a736e1b0526
nolim1t <hello@nolim1t.co>
2019-06-05 16:01:45 +0700

    Update package name

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 setup(
     name="nodebackup",
     version="0.0.1",
-    packages=["nodebackup"],
+    packages=["nodebackup-nolim1t"],
     install_requires=["toml", "dropbox"],
     entry_points={"console_scripts": ["nodebackup = nodebackup.nodebackup:main"]},
 
