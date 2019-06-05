46281370ea8e4adaaeb0be41cbc8b5038e27f445
nolim1t <hello@nolim1t.co>
2019-06-05 15:43:24 +0700

    Add licenses

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
+'''
+   Copyright 2019 nolim1t.co
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+'''
+
 from setuptools import setup
 
 setup(
@@ -14,5 +30,8 @@ setup(
     license="Apache 2.0",
     keywords="bitcoin lightning node backup",
     url="https://gitlab.com/nolim1t/nodebackup",
-
+    classifiers=[
+        "License :: OSI Approved :: Apache 2.0 License",
+        "Operating System :: UNIX Systems",        
+    ]
 )
