bbee3e27bc8d792d8026ab217ceb2651f78e8ee7
nolim1t <hello@nolim1t.co>
2019-06-08 12:37:59 +0700

    Make the file executable

--- nodebackup/nodebackup.py
+++ nodebackup/nodebackup.py
@@ -1,33 +1,18 @@
+#!/usr/bin/env python3
+
 """
 Usage: lndnodebackup start
 """
-
-'''
-   Copyright 2019 nolim1t.co
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-'''
-
 
 import os, sys, logging
 from docopt import docopt
 
 # Internal
-from configutils import readconfig
-from daemon import  startdaemon
+import nodebackup.lndbackupdaemon
+import nodebackup.configutils
 
 # Check for logfile (Used for general logging. This needs to be in entrypoint)
-configuration = readconfig()
+configuration = nodebackup.configutils.readconfig()
 if not 'logfile' in configuration:
     print("no logfile path defined!")
     sys.exit(1)
@@ -38,7 +23,7 @@ logging.basicConfig(filename=configuration['logfile'], level=logging.DEBUG, form
 def main():
     args = docopt(__doc__, version="v0.0.2")
     if args["start"]:
-        startdaemon()
+        nodebackup.lndbackupdaemon.startdaemon()
 
 if __name__ == '__main__':
     main()
